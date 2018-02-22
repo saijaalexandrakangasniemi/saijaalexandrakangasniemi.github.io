8a6c76e3ee9e8f812c016948695043673ac23682
Saija Kangasniemi <saijakangasniemi@byod-110-74.pub.uniarts.fi>
2018-02-22 12:06:50 +0200

    we added something

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -8,7 +8,23 @@ oriented = function(e) {
             compassHeading = e.alpha;
         }
         
-         text_area.textContent = compassHeading;
+if(compassHeading > 315 || compassHeading < 45) {
+                text_area.textContent = compassHeading;
+        }
+
+        if(compassHeading >= 45 && compassHeading < 135) {
+                text_area.textContent = "east 5";
+        }
+
+        if(compassHeading >= 135 && compassHeading < 225) {
+                text_area.textContent = "south 5";
+        }
+
+        if(compassHeading >= 225 && compassHeading < 315) {
+                text_area.textContent = "west 5";
+        }
+
+       //  text_area.textContent = compassHeading;
         //calls function to alter content based on heading
        // myOrientation();
     }
